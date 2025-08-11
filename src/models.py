8e495c5801685633991a74a060279c9d8d9c8f17
Luis Bermudez <44736658+luisbs2412@users.noreply.github.com>
2025-08-11 21:35:10 +0000

    modelo starwars terminado

--- src/models.py
+++ src/models.py
@@ -1,17 +1,50 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, DateTime, Float, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column
+import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    password: Mapped[str] = mapped_column(String(80), nullable=False)
+    nombre: Mapped[str] = mapped_column(String(30))
+    apellido: Mapped[str] = mapped_column(String(30))
+    fecha_subscripcion: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.now, nullable=False)
+
+class Personaje(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    genero: Mapped[str] = mapped_column(String(20))
+    altura: Mapped[float] = mapped_column(Float)
+    color_ojos: Mapped[str] = mapped_column(String(50))
+    color_cabello: Mapped[str] = mapped_column(String(50)) 
+
+class Planeta(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    diametro: Mapped[int] = mapped_column(Integer)
+    clima: Mapped[str] = mapped_column(String(50))
+    terreno: Mapped[str] = mapped_column(String(50))
+    poblacion: Mapped[int] = mapped_column(BigInteger)
+
+PersonajeFavorito = Table(
+    "personajeFavorito",
+    db.metadata,
+    Column("usuario_id", ForeignKey("user.id")),
+    Column("personaje_id", ForeignKey("personaje.id"))
+)  
+
+PlanetaFavorito = Table(
+    "planetaFavorito",
+    db.metadata,
+    Column("usuario_id", ForeignKey("user.id")),
+    Column("planeta_id", ForeignKey("planeta.id"))
+)
 
 
-    def serialize(self):
+def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
